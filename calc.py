dd23019b1d87ab281ff3b0f585d73df6b980ed15
samriddhi004 <karkisamriddhi04@gmail.com>
2025-12-14 22:46:47 +0545

    Change multiply function to accept three numbers and return their product

--- calc.py
+++ calc.py
@@ -4,8 +4,8 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
-    return a * b
+def multiply(a, b, c):
+    return a * b * c
 
 def divide(a, b):
     if b == 0:
@@ -14,5 +14,5 @@ def divide(a, b):
 if __name__ == "__main__":
     print(add(5, 3))
     print(subtract(10, 4))
-    print(multiply(6, 7))
+    print(multiply(6, 7, 8))
     print(divide(20, 5))
